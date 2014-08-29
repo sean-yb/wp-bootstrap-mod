ed4c858b1217b6b2e5f92f5bd591a65b359239e7
unknown <bethany@HP-8200-Elite.domain.local>
2014-08-29 16:58:59 -0400

    changed navbar style

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
 
 		<div role="banner">
 				
-			<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar navbar-default navbar-fixed-top">
 
 				<div class="container">
           
